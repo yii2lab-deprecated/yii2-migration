d624243830125f348808b038591c356062be2057
Vitaliy <theyamshikov@yandex.ru>
2017-09-12 12:49:48 +0600

    fix

--- src/helpers/MigrationHelper.php
+++ src/helpers/MigrationHelper.php
@@ -2,14 +2,18 @@
 
 namespace yii2lab\migration\helpers;
 
+use Yii;
 use yii2lab\helpers\Helper;
 use yii\helpers\ArrayHelper;
+use yii2lab\helpers\yii\FileHelper;
 
 class MigrationHelper {
 	
 	private static $aliases;
+	private static $config;
 	
 	public static function setPath($config) {
+		self::$config = $config;
 		if(APP != CONSOLE) {
 			return $config;
 		}
@@ -27,13 +31,36 @@ class MigrationHelper {
 		foreach($apps as $app) {
 			self::getAppMigrations($app);
 		}
-		return self::$aliases;
+		foreach(self::$config['params']['dee.migration.scan'] as $target) {
+			self::scanMigrations($target);
+		}
+		$aliases = array_unique(self::$aliases);
+		//prr($aliases,1,1);
+		return $aliases;
 	}
 
 	private static function addMigrationsDir($dir) {
 		if(is_dir($dir)) {
 			self::$aliases[] = '@' . $dir;
 		}
+	}
+	
+	private static function scanMigrations($path) {
+		$dir = Yii::getAlias($path);
+		$pathList = FileHelper::findFiles($dir);
+		foreach($pathList as $pathItem) {
+			if(strpos($pathItem, 'migrations') !== false) {
+				$alias = self::extractAlias($pathItem);
+				self::addMigrationsDir($alias);
+			}
+		}
+	}
+	
+	private static function extractAlias($pathItem) {
+		$dirName = dirname($pathItem);
+		$dirName = str_replace(ROOT_DIR . DS, '', $dirName);
+		$dirName = str_replace('\\', '/', $dirName);
+		return $dirName;
 	}
 	
 	private static function getAppMigrations($app) {
