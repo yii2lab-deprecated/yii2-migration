aa1828f6410601b0c8fd27a9333057258b85340a
Vitaliy <theyamshikov@yandex.ru>
2017-12-24 18:47:36 +0600

    refacto filter

--- src/domain/filters/SetPath.php
+++ src/domain/filters/SetPath.php
@@ -1,81 +1,80 @@
 <?php
 
-namespace yii2lab\migration\helpers;
+namespace yii2lab\migration\domain\filters;
 
 use common\enums\app\AppEnum;
 use Yii;
+use yii\base\BaseObject;
 use yii2lab\helpers\Helper;
-use yii\helpers\ArrayHelper;
 use yii2lab\helpers\ModuleHelper;
 use yii2lab\helpers\yii\FileHelper;
+use yii2lab\misc\interfaces\FilterInterface;
+use yii2mod\helpers\ArrayHelper;
 
-class MigrationHelper {
+class SetPath extends BaseObject implements FilterInterface {
+
+	private $aliases;
 	
-	private static $aliases;
-	private static $config;
-	
-	public static function setPath($config) {
-		self::$config = $config;
+	public function run($config) {
 		if(APP != CONSOLE) {
 			return $config;
 		}
 		$config['params']['dee.migration.path'] = ArrayHelper::merge(
 			ArrayHelper::getValue($config['params'], 'dee.migration.path', []),
-			self::getAliases()
+			$this->getAliases($config)
 		);
 		return $config;
 	}
 	
-	private static function getAliases() {
-		self::$aliases = [];
+	private function getAliases($config) {
+		$this->aliases = [];
 		$apps = AppEnum::values();
 		$apps = ArrayHelper::merge($apps, Helper::getApiSubApps());
 		foreach($apps as $app) {
-			self::getAppMigrations($app);
+			$this->getAppMigrations($app);
 		}
-		if(!empty(self::$config['params']['dee.migration.scan'])) {
-			$scanAliases = self::$config['params']['dee.migration.scan'];
+		if(!empty($config['params']['dee.migration.scan'])) {
+			$scanAliases = $config['params']['dee.migration.scan'];
 			if(!empty($scanAliases)) {
 				foreach($scanAliases as $target) {
-					self::scanMigrations($target);
+					$this->scanMigrations($target);
 				}
 			}
 		}
-		$aliases = array_unique(self::$aliases);
+		$aliases = array_unique($this->aliases);
 		return $aliases;
 	}
-
-	private static function addMigrationsDir($dir) {
+	
+	private function addMigrationsDir($dir) {
 		if(is_dir($dir)) {
-			self::$aliases[] = '@' . $dir;
+			$this->aliases[] = '@' . $dir;
 		}
 	}
 	
-	private static function scanMigrations($path) {
+	private function scanMigrations($path) {
 		$dir = Yii::getAlias($path);
 		$pathList = FileHelper::findFiles($dir);
 		foreach($pathList as $pathItem) {
 			if(strpos($pathItem, 'migrations') !== false) {
-				$alias = self::extractAlias($pathItem);
-				self::addMigrationsDir($alias);
+				$alias = $this->extractAlias($pathItem);
+				$this->addMigrationsDir($alias);
 			}
 		}
 	}
 	
-	private static function extractAlias($pathItem) {
+	private function extractAlias($pathItem) {
 		$dirName = dirname($pathItem);
 		$dirName = str_replace(ROOT_DIR . DS, '', $dirName);
 		$dirName = str_replace('\\', '/', $dirName);
 		return $dirName;
 	}
 	
-	private static function getAppMigrations($app) {
-		self::addMigrationsDir($app . '/migrations');
+	private function getAppMigrations($app) {
+		$this->addMigrationsDir($app . '/migrations');
 		$modules = ModuleHelper::allNamesByApp($app);
 		foreach($modules as $module) {
 			$dir = $app . '/modules/' . $module . '/migrations';
-			self::addMigrationsDir($dir);
+			$this->addMigrationsDir($dir);
 		}
 	}
-	
 }
